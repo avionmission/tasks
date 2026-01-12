9748401c30cc249db76482e81974760582569b49
Avinash Prasad <avionmission@gmail.com>
2026-01-13 01:14:15 +0530

    Fix: API BASE URL typo

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
