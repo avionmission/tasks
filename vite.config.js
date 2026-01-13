b524d53d00515bcfd3685df63bc7b225a9cc33ec
Avinash Prasad <avionmission@gmail.com>
2026-01-13 11:21:56 +0530

    Railway Deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tasks/",
+  base: "/",
+  preview: {
+    port: process.env.PORT || 4173,
+    host: true
+  }
 });
